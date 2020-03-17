d54ddd0c737f8a86dbd6a9315b66535c4cf05986
gong <379513080@qq.com>
2020-03-17 17:37:46 +0800

    ‘sha‘demo

--- goodsInfo/src/main/java/com/freekit/demo/mapper/GoodsInfoMapper.java
+++
@@ -1,7 +0,0 @@
-package com.freekit.demo.mapper;
-
-import com.freekit.demo.entity.GoodsInfo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GoodsInfoMapper extends JpaRepository<GoodsInfo,Long> {
-}
